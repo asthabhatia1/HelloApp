3234d5ffc52b0569b179ac8280e1bf79e6053b02
HarshaVardhanB05 <hb2592@srmist.edu.in>
2026-02-12 10:26:08 +0530

    Updated code

---
+++ Day1step.java
@@ -0,0 +1,8 @@
+class Day1step {
+    public static void main (String[] args){
+        System.out.println("Hello Step classes");
+         System.out.println("Hello Step classes");
+           System.out.println("Hello Step classes");
+    
+    }
+}
